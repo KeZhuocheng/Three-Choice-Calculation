1d231f58c89c00d8378c0a96aa89d0c086b617a3
KeZhuocheng <106873405+KeZhuocheng@users.noreply.github.com>
2022-07-07 22:42:16 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*int a=5;
+    int b=4;
+    int c;
+    c=a|b;
+
+    printf("%d",c);*/
+
+    int a=45;
+    int b=0;
+    int c=10;
+    c=(a+b,b+c,a+c);
+    printf("%d",c);
+
+    return 0;
+}
